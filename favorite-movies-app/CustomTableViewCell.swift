176f158c5d6494dfb3b3e8e3a47bf500830082bd
Zac Clark <zacac93@gmail.com>
2018-07-24 10:54:35 -0600

    initial commit

---
+++ favorite-movies-app/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  favorite-movies-app
+//
+//  Created by Zachary Clark on 7/23/18.
+//  Copyright © 2018 Zachary Clark. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ favorite-movies-app/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  favorite-movies-app
+//
+//  Created by Zachary Clark on 7/23/18.
+//  Copyright © 2018 Zachary Clark. All rights reserved.
+//
+
+import Foundation
